c4cdee3cdca9f9562a437712d0fd0015a63c68ce
alexous95 <alexandre95870@yahoo.fr>
2019-10-10 08:23:02 +0200

    Ajout du bon design pour la version portrait et correction de la version paysage

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - OUTLETS
+    
     @IBOutlet weak var shareView: ShareView!
     @IBOutlet weak var gridView: UIView!
     
@@ -27,12 +29,16 @@ class ViewController: UIViewController {
     let screenHeight = UIScreen.main.bounds.height
     let screenWidth  = UIScreen.main.bounds.width
     
+    var buttonPressed = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSwipeGesture()
         setupButton()
     }
 
+    
+    //MARK: - PRIVATE
     
     //This is the function called in the selector from the SwipeGesture initializer 
     @objc private func swipeViewUp( _ sender : UISwipeGestureRecognizer ){
@@ -59,13 +65,24 @@ class ViewController: UIViewController {
             }) { (success) in
                 if success {
                     //if the animation is successful we call another function in the completion closure
-                    print("c'est deja bien")
-                    self.backGridView()
+                    let renderer = UIGraphicsImageRenderer(size: self.gridView.bounds.size)
+                    let image = renderer.image { contex in
+                        self.gridView.drawHierarchy(in: self.gridView.bounds, afterScreenUpdates: true)
+                    }
+                    self.shareGridView(image: image)
                 }
             }
         default:
             break
         }
+    }
+    
+    private func shareGridView(image : UIImage){
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.completionWithItemsHandler = {(UIActivityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
+                self.backGridView()
+        }
+        present(activityController, animated: true, completion: nil)
     }
     
     //This function brings back the gridView to his original location with a dumping effect
@@ -75,7 +92,18 @@ class ViewController: UIViewController {
         
         UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.2, options: [], animations: {
             self.gridView.transform = .identity
+            self.originalGridView()
         }, completion: nil)
+    }
+    
+    //This functions brings back the grid view to its original state
+    private func originalGridView(){
+        self.topRectangleButton.setImage(UIImage(named: "plus"), for: .normal)
+        self.bottomRectangleButton.setImage(UIImage(named: "plus"), for: .normal)
+        self.bottomLeftButton.setImage(UIImage(named: "plus"), for: .normal)
+        self.bottomRightButton.setImage(UIImage(named: "plus"), for: .normal)
+        self.topLeftButton.setImage(UIImage(named: "plus"), for: .normal)
+        self.topRightButton.setImage(UIImage(named: "plus"), for: .normal)
     }
     
     private func setupSwipeGesture(){
@@ -108,10 +136,44 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func buttonWasPressed(_ sender : UIButton){
+    
+    private func changeLayout(layout : Int){
+           switch layout {
+           case 1:
+               self.topRectangleButton.isHidden = false
+               self.bottomLeftButton.isHidden = false
+               self.bottomRightButton.isHidden = false
+               self.topRightButton.isHidden = true
+               self.topLeftButton.isHidden = true
+               self.bottomRectangleButton.isHidden = true
+               
+           case 2:
+               self.topRectangleButton.isHidden = true
+               self.bottomLeftButton.isHidden = true
+               self.bottomRightButton.isHidden = true
+               self.topRightButton.isHidden = false
+               self.topLeftButton.isHidden = false
+               self.bottomRectangleButton.isHidden = false
+               
+           case 3:
+               self.topRectangleButton.isHidden = true
+               self.bottomLeftButton.isHidden = false
+               self.bottomRightButton.isHidden = false
+               self.topRightButton.isHidden = false
+               self.topLeftButton.isHidden = false
+               self.bottomRectangleButton.isHidden = true
+               
+           default:
+               print("oops")
+           }
+       }
+    
+    //MARK: - ACTIONS
+    
+    @IBAction func buttonLayoutWasPressed(_ sender : UIButton){
         switch sender.tag {
         case 10:
-           changeLayout(layout: 1)
+            changeLayout(layout: 1)
             sender.setImage(UIImage(named: "rectangleUpSelected"), for: .normal)
             layout2Button.setImage(UIImage(named: "layout2"), for: .normal)
             layout3Button.setImage(UIImage(named: "layout3"), for: .normal)
@@ -132,38 +194,56 @@ class ViewController: UIViewController {
         }
     }
     
-    private func changeLayout(layout : Int){
-        switch layout {
-        case 1:
-            self.topRectangleButton.isHidden = false
-            self.bottomLeftButton.isHidden = false
-            self.bottomRightButton.isHidden = false
-            self.topRightButton.isHidden = true
-            self.topLeftButton.isHidden = true
-            self.bottomRectangleButton.isHidden = true
-            
-        case 2:
-            self.topRectangleButton.isHidden = true
-            self.bottomLeftButton.isHidden = true
-            self.bottomRightButton.isHidden = true
-            self.topRightButton.isHidden = false
-            self.topLeftButton.isHidden = false
-            self.bottomRectangleButton.isHidden = false
-            
-        case 3:
-            self.topRectangleButton.isHidden = true
-            self.bottomLeftButton.isHidden = false
-            self.bottomRightButton.isHidden = false
-            self.topRightButton.isHidden = false
-            self.topLeftButton.isHidden = false
-            self.bottomRectangleButton.isHidden = true
-            
-        default:
-            print("oops")
+    @IBAction func buttonImagePickerWasPressed(_ sender : UIButton){
+        self.buttonPressed = sender
+        showImagePickerActionSheet()
+    }
+}
+
+
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    private func showImagePickerActionSheet(){
+        let photoLibraryAction = UIAlertAction(title: "Choose from Library", style: .default) { (action) in
+            self.showImagePickerController(sourceType: .photoLibrary)
         }
+        let cameraAction = UIAlertAction(title: "Take from Camera", style: .default) { (action) in
+            self.showImagePickerController(sourceType: .camera)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let alert = UIAlertController(title: "Choose an options", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(photoLibraryAction)
+        alert.addAction(cameraAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
+    /* We create a function that takes in parameter a source type from UIImagePickerController (.photoLibrary, .camera) and shows the image picker after a few configuration */
+    private func showImagePickerController( sourceType : UIImagePickerController.SourceType){
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = sourceType
+        present(imagePickerController, animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            self.buttonPressed.setImage(editedImage, for: .normal)
+        }
+        else if let originalImage = info[.originalImage] as? UIImage {
+            self.buttonPressed.setImage(originalImage, for: .normal)
+        }
+        else {
+            print("Pas d'immage")
+            return
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
     
 }
 
