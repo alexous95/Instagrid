31a7649544ebf2019ab3035f0a513df40d670ebe
alexous95 <alexandre95870@yahoo.fr>
2019-11-04 10:10:11 +0100

    Ajout du model

--- Instagrid/Model/ImageModel.swift
+++ Instagrid/Model/ImageModel.swift
@@ -11,7 +11,7 @@ import Foundation
 /// This class is used to manage an image from a model
 class ImageModel {
   
-  /// A dictionnary used to store the URL of the selected images. The key used to retrieve the URLs are the button's tag
+  /// A dictionnary used to store the URL of the selected images. The keys used to retrieve the URLs are the button's tag
   var imageDict: [Int : URL] = [:]
   
   /// This functions returns an URL for the specified tag
